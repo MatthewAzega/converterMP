550ca030095607ebf60004eb291b43d29a7001b4
Azega <56790000+MatthewAzega@users.noreply.github.com>
2023-05-19 18:10:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from pytube import YouTube
+from moviepy.editor import *
+
 def run():
     video_url = YouTube(str(input("please enter youtube url:")))
     video_name = video_url.title
-    #videoToAudioStream = video_url.streams.filter(only_audio=True)
     audioStream = video_url.streams.get_audio_only()
     audioStream.download()
-    print(f"Download completed ---> {video_name}")
+    print(f"Download completed")
+    convert_to_mp3(video_name)
+    print(f"Convert completed ---> {video_name}.mp3")
+
+def convert_to_mp3(output_file):
+    input_file = f"{output_file}.mp4"
+    output_file = f"{output_file}.mp3"
+    video = AudioFileClip(input_file)
+    video.write_audiofile(output_file)
 
 if __name__=='__main__':
     run()
